fd2f57f04e9fdca34ff74da911587692aa270cc1
naveenias <naveenacp24@gmail.com>
2024-05-21 22:14:42 +0530

    Added baseurl

--- client/src/apicalls/axiosInstance.js
+++ client/src/apicalls/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 
 export const axiosInstance =axios.create({
+    baseURL: 'https://naveenstutionbackend.vercel.app/',
     headers : {
         authorization : `Bearer ${localStorage.getItem("token")}`
     },
